c77466d4a30e768a1c4ebe4498fc695dd6200596
smallpotato000 <smallpotato000@hotmail.com>
2017-05-23 11:50:41 +0800

    it works!

--- 2.1.0/standalone/start_cluster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker create -ti --rm --name=namenode -h namenode.dockerspark.dev.docker --env-file ./master.env -p 8080:8080 -p 4040:4040 -p 50070:50070 -p 8088:8088 -p 19888:19888 geostar/dockerspark /etc/bootstrap.sh -nd
-
-docker create -ti --rm --name=datanode1 -h datanode1.dockerspark.dev.docker --link namenode:namenode.dockerspark.dev.docker --env SPARK_LOCAL_IP=datanode1.dockerspark.dev.docker geostar/dockerspark /etc/bootstrap.sh -dd
-
-docker create -ti --rm --name=datanode2 -h datanode2.dockerspark.dev.docker --link namenode:namenode.dockerspark.dev.docker --env SPARK_LOCAL_IP=datanode2.dockerspark.dev.docker geostar/dockerspark /etc/bootstrap.sh -dd
-
-
