34bce402f013ca768e7f8d9d145f5761a8d456f5
smallpotato000 <smallpotato000@hotmail.com>
2017-06-26 10:03:04 +0800

    add regionservers file. use fixed cluster size.

---
+++ 2.1.0/standalone/start.sh
@@ -0,0 +1,6 @@
+./startdns.sh
+./startmaster.sh
+./startslave.sh d1
+./startslave.sh d2
+./startslave.sh d3
+
---
+++ 2.1.0/standalone/stop.sh
@@ -0,0 +1,6 @@
+./stopmaster.sh
+./stopslave.sh d1
+./stopslave.sh d2
+./stopslave.sh d3
+./stopdns.sh
+
